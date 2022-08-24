32021d3a7bd397a176f67e75f5c956ed97c93057
edevaldohbf <edevaldohbfilho@gmail.com>
2022-08-24 12:14:22 -0300

    fix: ativando cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const settings = require('./settings/settings')
 const routes = require('./settings/routes')
 
@@ -6,6 +7,7 @@ const app = express();
 const host = settings.host;
 const port = settings.port;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
